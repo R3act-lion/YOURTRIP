730d3ca137f3158f85a080a6da26b5f6f8b9d2d3
ekdms5566 <ekdms5566@naver.com>
2022-12-20 16:19:56 +0900

    Feat : 이미지 모듈, 이미지 슬라이더 구현

---
+++ src/modules/Image/Image.jsx
@@ -0,0 +1,26 @@
+import styled  from "styled-components"
+
+const ImgDefault = styled.img`
+  object-fit: cover;
+  border-radius: ${props => props.borderRadius};
+  margin: ${props => props.margin};
+  width: ${props => props.width};
+  height: ${props => props.height};
+`
+
+const Image = ({
+  borderRadius,
+  margin,
+  width,
+  height,
+  src,
+  alt=""
+}) => {
+  return (
+    <ImgDefault
+      margin={margin} borderRadius={borderRadius} width={width} height={height} src={src} alt={alt} >
+    </ImgDefault>
+  )
+}
+
+export default Image
