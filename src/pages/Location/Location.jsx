dd611386d7fa4fd9868e94974f7a1c35a21f1c8a
coding-frog117 <syeshe2@gmail.com>
2022-12-20 20:21:02 +0900

    Fix : 라우터 연결 오류 수정

--- src/pages/Location/Location.jsx
+++ src/pages/Location/Location.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
 
 export default function Location() {
   return (
     <>
-    <Outlet />
     <div>지역별 페이지입니다.</div>
     </>
   )
