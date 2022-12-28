374f51f5cfe8bcc77ce5114affecf692faf397e8
coding-frog117 <syeshe2@gmail.com>
2022-12-28 23:50:04 +0900

    Feat : 프로필 팔로잉 유저 리스트 페이지 구현

---
+++ src/components/FollowingBtn/FollowingBtn.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import followBtn from '../../assets/images/btn-follow.svg'
+import unfollowBtn from '../../assets/images/btn-unfollow.svg';
+
+const ButtonFollow = styled.button`
+    width: 56px;
+    height: 28px;
+    background-image: url(${followBtn})
+`
+
+const ButtonCancel = styled.button`
+    width: 56px;
+    height: 28px;
+    background-image: url(${unfollowBtn})  
+`
+
+export default function FollowingBtn({item}){
+    const url= "https://mandarin.api.weniv.co.kr";
+    let token= localStorage.getItem('Access Token');
+    let [isFollow , setIsFollow]= useState(item.isfollow);
+
+    //팔로우 함수
+    const follow = async()=>{
+        try{
+        const res= await fetch(url+`/profile/${item.accountname}/follow`,{
+            method: "POST",
+            headers: {
+                    "Authorization" : `Bearer ${token}`,
+                    "Content-type" : "application/json"
+                }
+        }) 
+        const resJson= await res.json();
+        console.log(resJson);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+    
+    //언팔로우 함수
+    const unfollow = async()=>{
+        try{
+            const res= await fetch(url+`/profile/${item.accountname}/unfollow`,{
+                method: "DELETE",
+                headers: {
+                        "Authorization" : `Bearer ${token}`,
+                        "Content-type" : "application/json"
+                    }
+            }) 
+            const resJson= await res.json();
+            console.log(resJson);
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    return(
+        <>
+        {isFollow === true 
+            ? <ButtonCancel onClick={(e)=>{
+                setIsFollow(false);
+                unfollow();                                   
+            }} /> 
+            : <ButtonFollow onClick={(e)=>{
+                setIsFollow(true);
+                follow();                                   
+            }}/>}
+        </>
+    )
+}
