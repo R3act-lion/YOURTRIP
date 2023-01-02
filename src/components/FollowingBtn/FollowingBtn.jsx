d0ff1dd4ed7f912bf1b352a6e9cabfde48927e51
ekdms5566 <ekdms5566@naver.com>
2023-01-02 21:42:40 +0900

    Feat : 팔로워, 팔로잉 리스트 출력

--- src/components/FollowingBtn/FollowingBtn.jsx
+++ src/components/FollowingBtn/FollowingBtn.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 import Button from '../../modules/Button/Button';
 
 export default function FollowingBtn({ followState, followerCount, followingCount, userinfo }) {
     const url= "https://mandarin.api.weniv.co.kr";
     const token = localStorage.getItem('Access Token');
+    const path = useLocation().pathname;
     const [isFollow, setIsFollow] = useState(followState);
 
     useEffect(() => {
@@ -52,11 +54,11 @@ export default function FollowingBtn({ followState, followerCount, followingCoun
             {isFollow ?
                 <Button
                     text="언팔로우"
-                    color="#767676;"
+                    color="#767676"
                     backgroundColor="#FFF"
-                    width="120px"
-                    height="34px"
-                    fontSize="14px"
+                    width={path.includes('yourprofile') ? "120px" : "60px"}
+                    height={path.includes('yourprofile') ? "34px" : "28px"}
+                    fontSize={path.includes('yourprofile') ? "14px" : "12px"}
                     fontWeight="500"
                     className="btnChecked"
                     onClick={() => {
@@ -69,9 +71,9 @@ export default function FollowingBtn({ followState, followerCount, followingCoun
                     text="팔로우"
                     color="#FFF"
                     backgroundColor="#3C70BC"
-                    width="120px"
-                    height="34px"
-                    fontSize="14px"
+                    width={path.includes('yourprofile') ? "120px" : "60px"}
+                    height={path.includes('yourprofile') ? "34px" : "28px"}
+                    fontSize={path.includes('yourprofile') ? "14px" : "12px"}
                     fontWeight="500"
                     onClick={() => {
                         setIsFollow(true);
